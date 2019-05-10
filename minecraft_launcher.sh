6848eaa20cbe36ab5a390c488cd97cc3a4e3ae00
teflontoni <torben@mohrfeldt.net>
2019-05-10 18:54:05 +0200

    getting OP is driving me crazy

--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -8,11 +8,13 @@ mkdir -p "${work_dir}"
 touch "${work_dir}"/whitelist.json
 touch "${work_dir}"/server.properties
 touch "${work_dir}"/permissions.json
+touch "${work_dir}"/ops.json
 
 docker run -d -h ${server} --name=${server}\
  -v "${work_dir}/whitelist.json:/opt/minecraft/whitelist.json:z"\
  -v "${work_dir}/server.properties:/opt/minecraft/server.properties:z"\
- -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
+ -v "${work_dir}/ops.json:/opt/minecraft/ops.json:z"\
  -v "${work_dir}/permissions.json:/opt/minecraft/permissions.json:z"\
- --network=host\
+ -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
+  --network=host\
  teflontoni/minecraft-bedrockserver
