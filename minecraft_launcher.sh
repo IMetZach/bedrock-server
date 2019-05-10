1c1a63838eaff3217617114a92f84232ddfd90d1
teflontoni <torben@mohrfeldt.net>
2019-05-10 22:13:43 +0200

    If anyone can explain why my permissions.json gets ignored by docker I'd be forever grateful

--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -6,15 +6,13 @@ work_dir="${base_dir}"/"${server}"
 
 mkdir -p "${work_dir}"
 touch "${work_dir}"/whitelist.json
-touch "${work_dir}"/server.properties
 touch "${work_dir}"/permissions.json
-touch "${work_dir}"/ops.json
+touch "${work_dir}"/server.properties
 
 docker run -d -h ${server} --name=${server}\
  -v "${work_dir}/whitelist.json:/opt/minecraft/whitelist.json:z"\
- -v "${work_dir}/server.properties:/opt/minecraft/server.properties:z"\
- -v "${work_dir}/ops.json:/opt/minecraft/ops.json:z"\
  -v "${work_dir}/permissions.json:/opt/minecraft/permissions.json:z"\
+ -v "${work_dir}/server.properties:/opt/minecraft/server.properties:z"\ 
  -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
   --network=host\
  teflontoni/minecraft-bedrockserver
