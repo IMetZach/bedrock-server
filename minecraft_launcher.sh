1613f5e0b9df42e5eca402acd16b1df9a9917bcc
teflontoni <torben@mohrfeldt.net>
2019-05-11 08:29:09 +0200

    Update minecraft_launcher.sh

--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -10,9 +10,6 @@ touch "${work_dir}"/permissions.json
 touch "${work_dir}"/server.properties
 
 docker run -d -h ${server} --name=${server}\
- -v "${work_dir}/whitelist.json:/opt/minecraft/whitelist.json:z"\
- -v "${work_dir}/permissions.json:/opt/minecraft/permissions.json:z"\
- -v "${work_dir}/server.properties:/opt/minecraft/server.properties:z"\ 
- -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
-  --network=host\
+ -v "$(work_dir)":/opt/minecraft:z"\
+ --network=host\
  teflontoni/minecraft-bedrockserver
