38db1e1186f7c0c0864eb0ba2fc55343d273b0f8
Zach Detton <elliott@imetzach.com>
2020-06-30 14:27:32 -0400

    Updated the launcher script with new options

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a docker container to launch and run the Official [Minecraft Bedrock Edi
 Simply download the file minecraft_launcher.sh, edit the "base_dir" to where you'd like your server info stored, and the run it:
 
 ```
-sudo ./minecraft_launcher.sh <server name>
+./minecraft_launcher.sh --start <server name>
 ```
 
 Then on your server navigate to where ever you pointed the base_dir to and you'll see a folder named after your games and your config files in it. You can then do:
@@ -19,28 +19,18 @@ sudo docker ps
 to get the container name, which should be your server name and then do:
 
 ```
-sudo docker stop <container name>
+docker stop <container name>
 ```
 
 Edit your config files and then start the server back up with:
 
 ```
-sudo docker start <container name>
+docker start <container name>
 ```
 
 ## Upgrading to a newer version:
-First you'll need to stop and remove your old container image. The container uses volumes for your world and config so you don't have to worry about loosing it:
-
 ```
-sudo docker stop <container name>
-sudo docker container rm <container name>
-sudo docker image rm <image name>
-```
-
-now run the launcher script using the same server name as before. When the container starts up, it will already find the existing game folder with world, level, and config files. If your previous server was named "Billy", then you would use the following:
-
-```
-./minecraft_launcher.sh Billy
+./minecraft_launcher.sh --upgrade <server name>
 ```
 
 ## Related Links
--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-# VARIABLES
-server=$1
-base_dir="/var/games"
+################################
+####        VARIABLES       ####
+################################
+mode=$1
+server=$2
+server_image="docker.io/imetzach/bedrock-server:latest"
+base_dir="${HOME}/local/games/minecraft"
 work_dir="${base_dir}"/"${server}"
 
+################################
+####        FUNCTIONS       ####
+################################
+check_server(){
 # MAKE THE DIRECTORY AND FILES
-mkdir -p "${work_dir}"
-touch "${work_dir}"/whitelist.json
-touch "${work_dir}"/server.properties
-touch "${work_dir}"/permissions.json
+if [ -d ${work_dir} ]
+	then
+	printf "Server directory already exists\n"
+	else
+	printf "Creating World Directory and Files\n"
+	mkdir -p "${work_dir}/worlds"
+	touch "${work_dir}"/whitelist.json
+	touch "${work_dir}"/server.properties
+	touch "${work_dir}"/permissions.json
+fi
+}
 
+server_start(){
 # START THE CONTAINER
 docker run -d -h ${server} --name=${server}\
  -v "${work_dir}/whitelist.json:/opt/minecraft/whitelist.json:z"\
@@ -18,4 +34,29 @@ docker run -d -h ${server} --name=${server}\
  -v "${work_dir}/permissions.json:/opt/minecraft/permissions.json:z"\
  -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
  --network=host\
- imetzach/bedrock-server
+ ${server_image}
+}
+
+server_update(){
+docker pull ${server_image}
+docker restart ${server}
+}
+
+################################
+####       MAIN SCRIPT      ####
+################################
+case ${mode} in
+	--start)
+		check_server
+		server_start
+		;;
+	--upgrade)
+		server_update
+		;;
+	*)
+		printf "Usage: $0 [option] <server name>\n"
+		printf "Options:\n"
+		printf "\t --start: Start a new server\n"
+		printf "\t --upgrade: Upgrade and restart an existing server\n"
+		;;
+esac
